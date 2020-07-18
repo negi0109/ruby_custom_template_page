42359b6a0cc277586d1fc45e21a952add16b7ff3
negi0109 <negi0109.seg@gmail.com>
2020-07-19 00:33:15 +0900

    initial commit

---
+++ lib/ruby_custom_template_page/version.rb
@@ -0,0 +1,3 @@
+module RubyCustomTemplatePage
+  VERSION = "0.1.0"
+end
---
+++ spec/ruby_custom_template_page_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe RubyCustomTemplatePage do
+  it "has a version number" do
+    expect(RubyCustomTemplatePage::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
