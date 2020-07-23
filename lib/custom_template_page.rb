31c46dcbb8b43a5a86b17b3268fb1fcd99f7a492
negi0109 <negi0109.seg@gmail.com>
2020-07-23 18:32:40 +0900

    string variable convert

--- lib/custom_template_page.rb
+++ lib/custom_template_page.rb
@@ -3,4 +3,13 @@ require "custom_template_page/version"
 module CustomTemplatePage
   class Error < StandardError; end
   # Your code goes here...
+  def self.convert(source, datas)
+    tmp = source
+    datas.each do |k, v|
+      p [k, v]
+      tmp = tmp.gsub("@" + k.to_s, v)
+    end
+
+    tmp
+  end
 end
--- spec/custom_template_page_spec.rb
+++ spec/custom_template_page_spec.rb
@@ -2,4 +2,14 @@ RSpec.describe CustomTemplatePage do
   it "has a version number" do
     expect(CustomTemplatePage::VERSION).not_to be nil
   end
+
+  describe "convert" do
+    it "string data" do
+      source = "@a @b"
+      data = { a: "abc", b: "def" }
+
+      out = CustomTemplatePage.convert(source, data)
+      expect(out).to eq "abc def"
+    end
+  end
 end
